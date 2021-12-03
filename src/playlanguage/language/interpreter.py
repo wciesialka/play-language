4c597a09f6d3bab7d51e1829b81f486fe2129f03
Willow Ciesialka <wciesialka@gmail.com>
2021-12-03 15:37:24 -0500

    interpreter now has a better linting score.

--- src/playlanguage/language/interpreter.py
+++ src/playlanguage/language/interpreter.py
@@ -1,36 +1,47 @@
+'''Module containing the Interpreter class,\
+     which is responsible for running PlayLanguage programs.'''
+
+import logging
+from typing import List
 import playlanguage.language.tokenizer as tokenizer
 import playlanguage.language.tokens as tokens
-import logging
 
 class Interpreter:
+    '''PlayLanguage Interpreter class.'''
 
-    def __init__(self, raw_input:str):
-        self.stack = []
+    def __init__(self, raw_input: str):
+        self.stack: List[int] = []
         reader = tokenizer.Tokenizer(self.stack)
-        self.program = reader.read(raw_input)
-        self.i = 0
-        self.fi = []
-        self.return_stack = []
-        self.save_register = None
+        self.program: List[tokens.Token] = reader.read(raw_input)
+        self.i: int = 0
+        self.if_stack: List[bool] = []
+        self.return_stack: List[int] = []
+        self.save_register: int = None
 
-    def get(self):
+    def get(self) -> tokens.Token:
+        '''Get the current Token residing in the program at position "i"'''
         if self.i >= len(self.program):
             return None
         token = self.program[self.i]
         self.i = self.i + 1
         return token
 
-    def interpret(self,/,condition=True):
+    def interpret(self, /, condition: bool = True) -> bool:
+        '''Run a program.
+        :param condition: If we're in an if statement, was the condition true?
+        :type condition: bool
+        :returns: The unaltered condition.
+        :rtype: bool'''
 
         while not (operation := self.get()) is None:
 
             if condition:
-                logging.debug("Interpreting token %s",str(operation))
-                if isinstance(operation,tokens.IfToken):
-                    fi = self.interpret(condition = (self.stack[-1] != 0))
-                    self.fi.append(fi)
-                elif isinstance(operation,tokens.ElseToken):
-                    self.interpret(condition = not self.fi.pop())
+                logging.debug("Interpreting token %s", str(operation))
+                if isinstance(operation, tokens.IfToken):
+                    if_stack = self.interpret(condition=(self.stack[-1] != 0))
+                    self.if_stack.append(if_stack)
+                elif isinstance(operation, tokens.ElseToken):
+                    self.interpret(condition=(not self.if_stack.pop()))
                 elif isinstance(operation, tokens.ReturnToken):
                     self.return_stack.append(self.i-1)
                 elif isinstance(operation, tokens.JumpToken):
@@ -46,7 +57,7 @@ class Interpreter:
                 else:
                     value = operation()
                     if not value is None:
-                        print(value,end="")
+                        print(value, end="")
             if isinstance(operation, tokens.EndIfToken):
-                logging.debug("Interpreting token %s",str(operation))
+                logging.debug("Interpreting token %s", str(operation))
                 return condition
