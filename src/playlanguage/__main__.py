d52d8c3d95cb21dab2be782711165eb78ff1a1d9
Willow Ciesialka <wciesialka@gmail.com>
2021-12-03 15:20:44 -0500

    __main__.py now good linting score.

--- src/playlanguage/__main__.py
+++ src/playlanguage/__main__.py
@@ -1,10 +1,14 @@
 #!/usr/bin/env python3
+'''Handler for command line functionality.'''
 
 import argparse
 import logging
 from playlanguage.language.interpreter import Interpreter
 
-def main(args):
+def main(args: argparse.Namespace):
+    '''Main function.
+    :param args: Command line arguments.
+    :type args: argparse.Namespace'''
     source_code = args.input.read().strip()
 
     interpreter = Interpreter(source_code)
@@ -12,19 +16,23 @@ def main(args):
     print()
 
 def entry():
+    '''Command line entry point.'''
     levels = {
         "d": logging.DEBUG,
         "i": logging.INFO,
         "w": logging.WARNING,
         "e": logging.ERROR,
-        "c": logging.CRITICAL 
+        "c": logging.CRITICAL
     }
 
-    parser = argparse.ArgumentParser(prog="playlanguage",description="Interpreter for the Playlanguage.")
-    parser.add_argument("input",type=argparse.FileType("r"),help="Input file.")
-    parser.add_argument("-l","--level",type=str.lower,choices=levels.keys(),default='w',help="Logging level.")
+    parser = argparse.ArgumentParser(prog="playlanguage", \
+        description="Interpreter for the Playlanguage.")
+    parser.add_argument("input", type=argparse.FileType("r"), \
+        help="Input file.")
+    parser.add_argument("-l", "--level", type=str.lower, \
+        choices=levels.keys(), default='w', help="Logging level.")
     args = parser.parse_args()
-    
+
     logging.basicConfig(level=levels[args.level])
 
     try:
