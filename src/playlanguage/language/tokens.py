5efe999a8a282dfce22a0efb9d9308cfc485849e
Willow Ciesialka <wciesialka@gmail.com>
2021-12-03 15:50:12 -0500

    tokens now has a better linting score.

--- src/playlanguage/language/tokens.py
+++ src/playlanguage/language/tokens.py
@@ -1,16 +1,18 @@
+'''Module containing the TokenBuilder class and all Tokens.'''
+
 from abc import ABCMeta, abstractmethod
 import struct
 from typing import List
 
-MAX_BYTE_SIZE = 4
+MAX_BYTE_SIZE: int = 4
 
 class TokenBuilder:
     '''Factory for creating tokens.'''
 
-    def __init__(self, stack:List[int]):
+    def __init__(self, stack: List[int]):
         self._stack = stack
 
-def add_builder(name:str):
+def add_builder(name: str):
     '''This decorator allows us to add build_x methods to TokenBuilder using a decorator\
         instead of rewriting similar method headers every time.
         :param name: Name of the class.
@@ -20,13 +22,12 @@ def add_builder(name:str):
         raise ValueError(f"Attribute \"{name}\" already exists in TokenBuilder class.")
 
     def decorate(cls):
-        def func(self,*args):
-            return cls(self._stack,*args)
+        def func(self, *args):
+            return cls(self._stack, *args)
 
-        setattr(TokenBuilder,name,func)
+        setattr(TokenBuilder, name, func)
         return cls
 
-        
     return decorate
 
 class Token:
@@ -34,11 +35,12 @@ class Token:
 
     __metaclass__ = ABCMeta
 
-    def __init__(self, stack:List[int]):
+    def __init__(self, stack: List[int]):
         self.stack = stack
 
     @abstractmethod
     def func(self):
+        '''Perform the function of the Token.'''
         return
 
     def __call__(self, *args, **kwargs):
@@ -48,11 +50,12 @@ class Token:
         return self.__class__.__name__
 
 class BinaryOperation(Token):
-
+    '''Base Binary Operation abstract class.'''
     __metaclass__ = ABCMeta
 
     @abstractmethod
-    def operation(self, a:int, b:int):
+    def operation(self, a: int, b: int):
+        '''Perform the binary operation.'''
         return
 
     def func(self):
@@ -61,23 +64,25 @@ class BinaryOperation(Token):
         self.stack.append(self.operation(a, b))
 
 class UnaryOperation(Token):
-
+    '''Base Unary Operation abstract class.'''
     __metaclass__ = ABCMeta
 
     @abstractmethod
-    def operation(self, a:int):
+    def operation(self, a: int):
+        '''Perform the unary operation.'''
         return
-    
+
     def func(self):
         a = self.stack.pop()
         self.stack.append(self.operation(a))
 
 class Formatter(Token):
-
+    '''Base Formatter abstract class.'''
     __metaclass__ = ABCMeta
 
     @abstractmethod
-    def format(self, packed:bytes):
+    def format(self, packed: bytes):
+        '''Format the packed bytes into something new.'''
         return
 
     def func(self):
@@ -88,15 +93,16 @@ class Formatter(Token):
 # Special Tokens
 @add_builder("push")
 class PushToken(Token):
-
-    def __init__(self, stack:List[int], value:int):
+    '''Push a value onto the stack.'''
+    def __init__(self, stack: List[int], value: int):
         super().__init__(stack)
-        
+
         if abs(value) > (1 << (MAX_BYTE_SIZE*8)):
             raise ValueError(f"Value of \"{value}\" exceeds max byte size of {MAX_BYTE_SIZE}.")
         self.value = value
 
     def func(self):
+        '''Push our value.'''
         self.stack.append(self.value)
 
     def __str__(self):
@@ -104,101 +110,101 @@ class PushToken(Token):
 
 @add_builder("pop")
 class PopToken(Token):
-
+    '''Pop from the top of the stack and return it.'''
     def func(self):
         return self.stack.pop()
 
 @add_builder("peek")
 class PeekToken(Token):
-
+    '''Return what's on top of the stack.'''
     def func(self):
         return self.stack[-1]
 
 @add_builder("empty")
 class EmptyToken(Token):
-
+    '''Empty the stack.'''
     def func(self):
         del self.stack[:]
 
 @add_builder("non_op")
 class NonOpToken(Token):
-
+    '''Do nothing.'''
     def func(self):
         pass
 
 @add_builder("chr")
 class ChrToken(Token):
-
+    '''Peek at the top of the stack, but as a character.'''
     def func(self):
         return chr(self.stack[-1])
 
 @add_builder("copy")
 class CopyToken(Token):
-
+    '''Push the value at the top of the stack onto the stack.'''
     def func(self):
         self.stack.append(self.stack[-1])
 
 @add_builder("if")
 class IfToken(Token):
-
+    '''If token.'''
     def func(self):
         pass
 
 @add_builder("endif")
 class EndIfToken(Token):
-
+    '''End if token.'''
     def func(self):
         pass
 
 @add_builder("else")
 class ElseToken(Token):
-
+    '''Else token.'''
     def func(self):
         pass
 
 @add_builder("return")
 class ReturnToken(Token):
-
+    '''Set a return point.'''
     def func(self):
         pass
 
 @add_builder("jump")
 class JumpToken(Token):
-
+    '''Jump to return point.'''
     def func(self):
         pass
 
 @add_builder("conditional_jump")
 class ConditionalJumpToken(Token):
-
+    '''Conditional jump to return point.'''
     def func(self):
         pass
 
 @add_builder("save")
 class SaveToken(Token):
-
+    '''Save to register.'''
     def func(self):
         pass
 
 
 @add_builder("load")
 class LoadToken(Token):
-
+    '''Load from register.'''
     def func(self):
         pass
 
 # Unary Operation Tokens
 @add_builder("not")
 class NotToken(UnaryOperation):
-
+    '''If true, push 0, if false, push 1'''
     def operation(self, a: int):
         if a:
             return 0
-        else:
-            return 1
+        return 1
 
 @add_builder("negate")
 class NegateToken(UnaryOperation):
+    '''Push the negative of the value on top of the stack.'''
 
     def operation(self, a: int):
         return -a
@@ -207,48 +213,56 @@ class NegateToken(UnaryOperation):
 
 @add_builder("add")
 class AddToken(BinaryOperation):
+    '''Add the two numbers on top of the stack.'''
 
     def operation(self, a: int, b: int):
         return a + b
 
 @add_builder("subtract")
 class SubtractToken(BinaryOperation):
+    '''Subtract the two numbers on top of the stack.'''
 
     def operation(self, a: int, b: int):
         return a - b
 
 @add_builder("multiply")
 class MultiplyToken(BinaryOperation):
+    '''Multiply the two numbers on top of the stack.'''
 
     def operation(self, a: int, b: int):
         return a * b
 
 @add_builder("divide")
 class DivideToken(BinaryOperation):
+    '''Divide the two numbers on top of the stack.'''
 
     def operation(self, a: int, b: int):
         return a//b
 
 @add_builder("modulo")
 class ModuloToken(BinaryOperation):
+    '''Modulo the two numbers on top of the stack.'''
 
     def operation(self, a: int, b: int):
         return a % b
 
-@add_builder("and")
-class AndToken(BinaryOperation):
+@add_builder("band")
+class BandToken(BinaryOperation):
+    '''Bitwise and the two numbers on top of the stack.'''
 
     def operation(self, a: int, b: int):
         return a & b
 
-@add_builder("or")
-class OrToken(BinaryOperation):
+@add_builder("bor")
+class BorToken(BinaryOperation):
+    '''Bitwise or the two numbers on top of the stack.'''
 
     def operation(self, a: int, b: int):
         return a | b
 
 @add_builder("equality")
 class EqualityToken(BinaryOperation):
+    '''If the two numbers on top of the stack are equal, push 1. 0 otherwise.'''
 
     def operation(self, a: int, b: int):
         if a == b:
@@ -257,6 +271,8 @@ class EqualityToken(BinaryOperation):
 
 @add_builder("greater_than")
 class GreaterThanToken(BinaryOperation):
+    '''Pop two numbers from the stack. If the first is greater than the \
+        second, push 1. Push 0 otherwise.'''
 
     def operation(self, a: int, b: int):
         if a > b:
@@ -265,6 +281,9 @@ class GreaterThanToken(BinaryOperation):
 
 @add_builder("less_than")
 class LessThanToken(BinaryOperation):
+    '''Pop two numbers from the stack. If the first is less than the \
+        second, push 1. Push 0 otherwise.'''
+
 
     def operation(self, a: int, b: int):
         if a < b:
@@ -274,6 +293,8 @@ class LessThanToken(BinaryOperation):
 # Formatter Tokens
 @add_builder("tostring")
 class ToStringToken(Formatter):
-    def format(self, packed:bytes):
+    '''Turn the stack into a string.'''
+
+    def format(self, packed: bytes):
         unpacked = struct.unpack_from(f'{len(packed)}s', buffer=packed)[0]
         return unpacked.decode('utf-32')
